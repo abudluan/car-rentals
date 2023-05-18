5c554f8cd7fe52db51ea53ee14f784e82c2c51e3
abudluan <abudluan@gmail.com>
2023-05-18 11:09:07 -0300

    feat: img added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import ScrollToTop from './ScrollToTop';
 import NavigationBar from './components/NavigationBar/NavigationBar';
+
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,7 +13,7 @@ function App() {
       <ScrollToTop />
       <NavigationBar />
       <Routes>
-
+      <Route exact path='/' element={<Home/>} />
       </Routes>
     </Router>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './Home.scss';
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+import CarSec1 from './img/Home/car-sec1.png';
+
+const Home = () => {
+    return (
+        <section id='home'>
+
+            <Container className="sec1">
+                <Row>
+
+                    <Col>
+                        <Container className="text-Carro">
+
+                           <h4>Planeje sua viagem agora</h4>
+                           <h1>Economize <span className="markText">muito</span> com o nosso aluguel de carros</h1>
+                           <p>Alugue o carro dos seus sonhos. Preços imbatíveis, milhas ilimitadas, opções de retirada flexíveis e muito mais.</p>
+                            
+                            <Col>
+                            <Button>Reserve </Button>
+                            </Col>
+
+                            <Col>
+                            </Col>
+                           
+                        </Container>
+                    </Col>
+
+                    <Col className="img-Carro"><img src={CarSec1} className="img-fluid" alt="" /></Col>
+                </Row>
+            </Container>
+
+        </section>
+    );
+}
+
+export default Home;
