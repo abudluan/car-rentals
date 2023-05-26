d16738a15ed0a06817ea6da811bc4edad4abaaa0
abudluan <abudluan@gmail.com>
2023-05-26 12:49:27 -0300

    feat: footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import './Footer.scss';
-
+import { BsTelephoneFill } from 'react-icons/bs';
+import { IoMdMail } from 'react-icons/io';
+import { FaCode } from 'react-icons/fa';
 const Footer = () => {
     return (
         <footer id="footer">
@@ -9,23 +11,40 @@ const Footer = () => {
 
                 <Row className="pt-4">
 
-                    <Col>
-                        1 of 3
+                    <Col md className="mb-4">
+                        <h2>Car Rentals</h2>
+                        <p>Entre em contato conosco.</p>
+
+                        <BsTelephoneFill className="mx-2" /> (99) 9999-9999
+                        <br />
+                        <IoMdMail className="mx-2" /> carrental@gmail.com
+
                     </Col>
 
-                    <Col>
-                        2 of 3
+                    <Col md className="mb-4">
+                        <h2>Funcionamento</h2>
+                        <p>
+                            Seg - Sex: 8:00 - 18:00
+                            <br />
+                            Sab: 9:00 - 17:00
+                            <br />
+                            Dom: Fechado
+                        </p>
                     </Col>
 
-                    <Col>
-                        3 of 3
-                    </Col>
+                    <Col md>
+                        <h2>Inscreva-se</h2>
+                        <p>Inscreva-se por e-mail e receba as últimas novidades e promoções</p>
 
-                    <Col>
-                        3 of 3
+                        
+                        <input className="emailinput" type="email" placeholder="Endereço de Email"></input>
+                        <br/>
+                        <Button className="btnEnviar mt-2">Enviar</Button>
                     </Col>
 
                 </Row>
+
+                <FaCode /> Desenvolvido por <a href="https://euabud.netlify.app/" target="_blank">Luan Abud</a>
 
             </Container>
         </footer>
